c79527d947aa2e615c32d17ded447b4c9842e887
Bruno Bernardo Henriques Cardoso <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2023-08-10 19:29:37 -0300

    Verifica se o número digitado é par ou impar

---
+++ impar_ou_par.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+    int n1;
+    cout<<"Digite um numero";
+    cin>>n1;
+       if(n1%2){
+           cout<<"O numero "<<n1<<" e impar";
+       }
+       else{
+           cout<<"O numero "<<n1<<" e par";
+       }
+       
+    return 0;
+}
