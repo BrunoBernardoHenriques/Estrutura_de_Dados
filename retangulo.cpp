131e998ad169ee408d6cf2220fa3fd43490da346
Bruno Bernardo Henriques Cardoso <51165192+BrunoBernardoHenriques@users.noreply.github.com>
2023-08-10 18:29:07 -0300

    Calcula area de um retângulo

---
+++ retangulo.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    
+    int base,altura,area;
+    cout<<"Digite a base do retangulo";
+    cin>>base;
+      cout<<"Digite a altura do retangulo";
+      cin>>altura;
+         area=altura*base;
+         
+         cout<<"A area do retangulo: "<<area;
+    return 0;
+}
